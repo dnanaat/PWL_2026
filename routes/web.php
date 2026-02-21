daad5c2499a30b77b79c5dc6de103802eef62009
Daana Hafidhil Islam <daanahafidhilislam@gmail.com>
2026-02-21 11:52:18 +0700

    Jobsheet 2 - Praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,32 @@
 <?php
 
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PhotoController; 
 
-Route::get('/hello', function () {
-    return ('Hello World');
-});
+Route::resource('photos', PhotoController::class);
+
+Route::get('/hello', [WelcomeController::class,'hello']);
+
+Route::get('/', HomeController::class);
+
+Route::get('/about', AboutController::class);
+
+Route::get('/articles/{id}', ArticleController::class);
+
+Route::resource('photos', PhotoController::class)->only([ 
+'index', 'show'
+]); 
+Route::resource('photos', PhotoController::class)->except([ 
+'create', 'store', 'update', 'destroy' 
+]);
 
 Route::get('/world', function () {
     return ('World');
-});
-
-Route::get('/', function () {
-    return ('Selamat Datang');
-});
-
-Route::get('/about', function () {
-    return ('244107020124 / Daana Hafidhil Islam');
 });
 
 Route::get('/user/{name}', function ($name) { 
@@ -25,11 +36,6 @@ return 'Nama saya '.$name;
 Route::get('/posts/{post}/comments/{comment}', function 
 ($postId, $commentId) { 
 return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
-});
-
-Route::get('/articles/{id}', function 
-($id) { 
-return 'Halaman Artikel dengan ID '.$id; 
 });
 
 Route::get('/user/{name?}', function ($name='John') { 
